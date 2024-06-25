6e2227f6facbdcd44daa3a1c0195f4d833092b2b
tung <tung.bui2004lk@hcmut.edu.vn>
2024-06-25 09:33:24 +0700

    add mob

--- src/main/java/com/example/java2dgame/entity/Player.java
+++ src/main/java/com/example/java2dgame/entity/Player.java
@@ -28,6 +28,7 @@ public class Player extends Entity{
         y = 100;
         speed = 4;
         direction = "down";
+        spriteNum = 1;
     }
     public void update()
     {
@@ -68,16 +69,56 @@ public class Player extends Entity{
         {
             y = 576 - titleSize*2;
         }
+        spriteCounter++;
+        if(spriteCounter > 12)
+        {
+           if(spriteNum == 1)
+           {
+               spriteNum = 2;
+           }
+           else if (spriteNum == 2)
+           {
+               spriteNum = 1;
+           }
+        }
+
     }
     public void draw(Graphics2D g2)
     {
         BufferedImage image = null;
-        image = up1;
+        if(direction == "up")
+        {
+            if(spriteNum == 1) image = up1;
+            else image = up2;
+        }
+        else if (direction == "down")
+        {
+            if(spriteNum == 1 )image = down1;
+            else image = down2;
+        }
+        else if (direction == "left")
+        {
+            if(spriteNum == 1 )image = left1;
+            else image = left2;
+        }
+        else if (direction == "right")
+        {
+            if(spriteNum == 1 )image = right1;
+            else image = right2;
+        }
+
         g2.drawImage(image,x,y,titleSize*2,titleSize*2,null);
     }
     public void getPlayerImage() throws IOException {
-        up1 = ImageIO.read(getClass().getResourceAsStream("/com/example/java2dgame/player/knight.png"));
 
+        up1 = ImageIO.read(getClass().getResourceAsStream("/com/example/java2dgame/player/boy_up_1.png"));
+        up2 = ImageIO.read(getClass().getResourceAsStream("/com/example/java2dgame/player/boy_up_2.png"));
+        down1 = ImageIO.read(getClass().getResourceAsStream("/com/example/java2dgame/player/boy_down_1.png"));
+        down2 = ImageIO.read(getClass().getResourceAsStream("/com/example/java2dgame/player/boy_down_2.png"));
+        left1 = ImageIO.read(getClass().getResourceAsStream("/com/example/java2dgame/player/boy_left_1.png"));
+        left2 = ImageIO.read(getClass().getResourceAsStream("/com/example/java2dgame/player/boy_left_2.png"));
+        right1 = ImageIO.read(getClass().getResourceAsStream("/com/example/java2dgame/player/boy_right_1.png"));
+        right2 = ImageIO.read(getClass().getResourceAsStream("/com/example/java2dgame/player/boy_right_2.png"));
 
     }
 }
